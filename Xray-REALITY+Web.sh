3b07572e701ecef1700e4b215b813684111259d8
bluejerry2021 <94281900+bluejerry2021@users.noreply.github.com>
2023-10-15 11:47:11 +0800

    Update Xray-REALITY+Web.sh

--- Xray-REALITY+Web.sh
+++ Xray-REALITY+Web.sh
@@ -17,7 +17,7 @@ unset nproc
 
 
 # 安装配置（nginx、openssl、PHP、cloudreve、xray的安装版本、位置等信息）
-nginx_version="nginx-1.25.1"
+nginx_version="nginx-1.24.0"
 openssl_version="openssl-openssl-3.1.3"
 nginx_prefix="/usr/local/nginx"
 nginx_config="${nginx_prefix}/conf.d/xray.conf"
